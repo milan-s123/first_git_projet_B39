6320a0335ab9a4ce9801de6e7ef3d3fab3f0549a
Milan <161379972+milan-s123@users.noreply.github.com>
2025-03-16 13:04:51 +0100

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,6 +3,10 @@ package day01;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        
         System.out.println("Hello World");
+        System.out.println("Hello Cyeo");
+        System.out.println("Hello Programmers");
+        
     }
 }
